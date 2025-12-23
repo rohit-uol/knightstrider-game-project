54b3643ef7a8568c11650fc8cf79538720577079
maxsroka <36799862+maxsroka@users.noreply.github.com>
2025-12-23 18:06:13 +0100

    added basic player movement

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Movement : MonoBehaviour
+namespace KnightStrider
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Movement : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Rigidbody2D rb;
+        Vector2 input;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void OnMove(InputValue value)
+        {
+            input = value.Get<Vector2>();
+
+            if (!Mathf.Approximately(input.y, 0f))
+            {
+                input.x = 0;
+            }
+            else if (!Mathf.Approximately(input.x, 0f))
+            {
+                input.y = 0;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
+        }
     }
 }
