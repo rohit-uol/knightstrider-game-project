1c08b47d044747af9f06d88ddbe19a0149f67cc4
maxsroka <36799862+maxsroka@users.noreply.github.com>
2026-02-25 19:15:50 +0100

    armor sounds

--- Assets/SFX/StepsSFX.cs
+++ Assets/SFX/StepsSFX.cs
@@ -4,19 +4,46 @@ namespace TheMasterPath
 {
     public class StepsSFX : MonoBehaviour
     {
-        [SerializeField] AudioSource source;
+        [SerializeField] AudioSource footSource;
+        [SerializeField] AudioSource armorSource;
         [SerializeField] AudioClip footsteps;
         [SerializeField] Movement movement;
+        [SerializeField] AudioClip[] armorClips;
+        int lastRandomArmorClipIndex = 0;
 
         void Start()
         {
-            movement.StepEnded += OnStepEnded;    
+            movement.StepEnded += OnStepEnded;
+        }
+        void OnStepEnded(Vector2 start, Vector2 end)
+        {
+            PlayFootSound();
+            PlayArmorSound();
         }
 
-        private void OnStepEnded(Vector2 start, Vector2 end)
+        void PlayFootSound()
         {
-            source.pitch = Random.Range(0.8f, 1.2f);
-            source.PlayOneShot(footsteps);
+            footSource.pitch = Random.Range(0.8f, 1.2f);
+            footSource.PlayOneShot(footsteps);
+        }
+
+        void PlayArmorSound()
+        {
+            var armorClipIndex = ChooseRandomArmorClipIndex();
+            armorSource.Stop();
+            armorSource.PlayOneShot(armorClips[armorClipIndex]);
+            lastRandomArmorClipIndex = armorClipIndex;
+        }
+
+        int ChooseRandomArmorClipIndex()
+        {
+            var armorClipIndex = Random.Range(0, armorClips.Length);
+            if (armorClipIndex == lastRandomArmorClipIndex)
+            {
+                armorClipIndex = (armorClipIndex + 1) % armorClips.Length;
+            }
+
+            return armorClipIndex;
         }
     }
 }
